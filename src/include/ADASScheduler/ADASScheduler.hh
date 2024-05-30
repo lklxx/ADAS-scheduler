96025bd86482dc0bb7695a8dfb044488876ca9a4
lklxx <b07705049@ntu.edu.tw>
2024-05-30 16:58:23 +0800

    Add basic workflow

--- src/include/ADASScheduler/ADASScheduler.hh
+++ src/include/ADASScheduler/ADASScheduler.hh
@@ -2,6 +2,18 @@ class ADASScheduler {
 public:
   ADASScheduler() {}
 
+  void parse_input_data(std::string input_data) {
+    std::cout << input_data << std::endl;
+  }
+
+  void simulated_annealing(int exec_time) {
+    std::cout << exec_time << std::endl;
+  }
+
+  void output_result(std::string output_file) {
+    std::cout << output_file << std::endl;
+  }
+
 private:
 
 };
--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,15 @@
 
 int main(int argc, char *argv[]) {
   cmdline::parser parser;
+  parser.add<std::string>("input", 'i', "input data with test case", true, "");
+  parser.add<std::string>("output", 'o', "output file for scheduling result", false, "");
+  parser.add<int>("time", 't', "execution time for simulated annealing", false, 15);
   parser.parse_check(argc, argv);
 
   ADASScheduler scheduler;
+  scheduler.parse_input_data(parser.get<std::string>("input"));
+  scheduler.simulated_annealing(parser.get<int>("time"));
+  scheduler.output_result(parser.get<std::string>("output"));
 
   return 0;
 }
